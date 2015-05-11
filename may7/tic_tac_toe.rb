84685c0401cc6255351c0938c6bb7e8778c254f2
Anthony Franco <arfranco@Anthonys-MacBook-Pro.local>
2015-05-11 02:15:48 -0400

    added a function so that human player can play computer or another human

--- may7/tic_tac_toe.rb
+++ may7/tic_tac_toe.rb
@@ -1,4 +1,3 @@
-require 'pry'
 require 'set'
 
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
@@ -25,8 +24,6 @@ def show_board(board)
   puts "                 "
   puts "                 "
   puts "                 "
-
-
 end
 
 def greeting(board)
@@ -56,6 +53,7 @@ def two_player?(board)
   if user_input_two_player == "yes" || user_input_two_player == "y" 
     puts "Awesome! Decide who goes first and have fun!" 
     show_board(board)
+    player_vs_player(board)
   elsif user_input_two_player == "no"
     cpu_player?(board)
   elsif user_input_two_player =~ /\d/
@@ -73,6 +71,7 @@ user_input_cpu_player = gets.chomp.downcase
     if user_input_cpu_player == "yes" || user_input_cpu_player =="y"
       puts "Great! Let's get started"
       show_board(board)
+      computer_play(board)
     elsif user_input_cpu_player =~ /\d/
       puts "Whoops! Looks like you typed in a number instead of yes or no. Would you like to play tic tac toe with me the computer? ('y' or 'n')"
       user_input_cpu_player = gets.chomp.downcase
@@ -84,8 +83,12 @@ user_input_cpu_player = gets.chomp.downcase
     end
 end
 
-def game_finished?(user1_numbers, user2_numbers)
+def humans_game_finished?(user1_numbers, user2_numbers)
   win?(user1_numbers) || win?(user2_numbers)
+end
+
+def comp_game_finished?(user1_numbers, comp_numbers)
+  win?(user1_numbers) || win?(comp_numbers)
 end
 
 def game_tied?(turn_count)
@@ -119,12 +122,19 @@ def player_two_turn(board, player_two_move)
   show_board(board)
 end
 
-def tic_tac_toe(board)
-  greeting(board)
+def computer_move(board, comp_numbers, user1_numbers)
+  if board.sample != comp_numbers || board.sample != user1_numbers
+    board[board.sample.to_i] = "O"
+    show_board(board)
+  end
+end
+
+
+def player_vs_player(board)
   turn_count = 9
   user1_numbers = Set.new
   user2_numbers = Set.new
-  until game_finished?(user1_numbers, user2_numbers) || game_tied?(turn_count) 
+  until game_finished?(user1_numbers, comp_numbers) || game_tied?(turn_count) 
     puts "What is your move player 1?:"
     player_one_move = gets.chomp.to_i
     player_one_turn(board, player_one_move)
@@ -152,5 +162,36 @@ def tic_tac_toe(board)
   end
 end
 
-binding.pry
+def computer_play(board)
+  turn_count = 9
+  user1_numbers = Set.new
+  comp_numbers = Set.new
+  until comp_game_finished?(user1_numbers, comp_numbers) || game_tied?(turn_count) 
+    puts "What is your move player 1?:"
+    player_one_move = gets.chomp.to_i
+    player_one_turn(board, player_one_move)
+    user1_numbers.add(player_one_move)
+    turn_count -= 1
+  if win?(user1_numbers) 
+    puts "Congratulations!! You won player 1!"
+    break
+    elsif game_tied?(turn_count) 
+      puts "Awww seems like there's a tie!"
+      break
+  end 
+    puts "The computer is making a move now."
+    comp_move1 = computer_move(board, comp_numbers, user1_numbers)
+    comp_numbers.add(comp_move1)
+    turn_count -= 1  
+  if win?(comp_numbers) 
+    puts "Congratulations!! You won player 2!"
+    break
+    elsif game_tied?(turn_count) 
+      puts "Awww seems like there's a tie!"
+      break 
+    end
+  end
+end
+
+greeting(board)
 
